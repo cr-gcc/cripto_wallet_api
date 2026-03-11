3cee7d936a731d557d5b7a3915353dd0a8001c2b
cris-contact-cinko <cris.gcc@outlook.com>
2026-03-10 18:22:20 -0600

    Detalles

--- app/Http/Requests/Wallet/StoreRequest.php
+++ app/Http/Requests/Wallet/StoreRequest.php
@@ -6,34 +6,34 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
+	/**
+	 * Determine if the user is authorized to make this request.
+	 */
+	public function authorize(): bool
+	{
+		return true;
+	}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
-        return [
-            'symbol' => 'required|string',
-            'amount' => 'required|numeric',
-						'avg_price' => 'nullable|numeric',
-        ];
-    }
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+	 */
+	public function rules(): array
+	{
+		return [
+			'symbol' => 'required|string',
+			'amount' => 'required|numeric',
+			'avg_price' => 'nullable|numeric',
+		];
+	}
 
-		public function messages()
-		{
-			return [
-				'symbol.required' => 'El símbolo es requerido',
-				'amount.required' => 'La cantidad es requerida',
-				'avg_price.required' => 'El precio promedio es requerido',
-			];
-		}
+	public function messages()
+	{
+		return [
+			'symbol.required' => 'El símbolo es requerido',
+			'amount.required' => 'La cantidad es requerida',
+			'avg_price.required' => 'El precio promedio es requerido',
+		];
+	}
 }
